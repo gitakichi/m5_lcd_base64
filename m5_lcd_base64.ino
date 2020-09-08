06472c1c6b077dbddbc711efad65bc79f0a6a46f
gitakichi <s.giakichi@gmail.com>
2020-09-08 13:48:50 +0900

    中身は取れてないけど受信できた

--- m5_lcd_base64.ino
+++ m5_lcd_base64.ino
@@ -1,8 +1,18 @@
 #include <M5StickC.h>               // M5StickCの読み込み
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "secret.h"//WIFIとMQTTのパスワード等を記載
 
 // イメージデータ
 #include "data.h"                   // 画像データの読み込み
 
+// クライアントIDをランダム生成するための文字列
+static const char alphanum[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+void reConnect();
 int base64_to_6bit();
 int base64Decode_ppm();
 
@@ -13,6 +23,17 @@ void setup() {
   M5.Lcd.setRotation(3);            // 0-3で画面の向き
   M5.Lcd.setSwapBytes(true);        // スワップON(色がおかしい場合には変更する)
 
+
+  Serial.print("WIFI Connecting ");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println(" CONNECTED");
+
+  mqttClient.setServer(server, 1883);
+
   unsigned short fixed[2940];
   base64Decode_ppm(b64_str, fixed);
 
@@ -22,7 +43,31 @@ void setup() {
 }
 
 void loop() {
+  reConnect();
+  mqttClient.loop();
+}
 
+void reConnect() { // 接続が切れた際に再接続する
+  char clientID[10];
+
+  while (!mqttClient.connected()) {// 接続まで繰り返す
+    Serial.print("MQTT Connecting ");
+    // Generate ClientID
+    for (int i = 0; i < 8; i++) {
+      clientID[i] = alphanum[random((sizeof(alphanum)))];
+    }
+
+    // MQTT broker に接続する
+    if (mqttClient.connect(clientID, username, NULL)) {
+      Serial.println(" CONNECTED");
+      mqttClient.subscribe(topic);
+      mqttClient.setCallback(callback);
+    }
+    else {//error and retry
+      Serial.print(".");
+      delay(5000);
+    }
+  }
 }
 
 //http://yamatyuu.net/computer/program/vc2013/base64/index.html
@@ -47,9 +92,9 @@ int base64Decode_ppm(const char* src, unsigned short *dtc) {
   while (*src != '\0') {
     //Base64 to HEX
     o0 = base64_to_6bit(*src);
-    o1 = base64_to_6bit(*(src+1));
-    o2 = base64_to_6bit(*(src+2));
-    o3 = base64_to_6bit(*(src+3));
+    o1 = base64_to_6bit(*(src + 1));
+    o2 = base64_to_6bit(*(src + 2));
+    o3 = base64_to_6bit(*(src + 3));
 
     h0 = (o0 << 2) | ((o1 & 0x30) >> 4);
     h1 = ((o1 & 0xf) << 4) | ((o2 & 0x3c) >> 2);
@@ -63,7 +108,14 @@ int base64Decode_ppm(const char* src, unsigned short *dtc) {
 
     //address increment
     dtc++;
-    src+=4;
+    src += 4;
   }
   return 0;
+}
+
+// メッセージを受け取ったらシリアルにプリント
+void callback(char* topic, byte* payload, unsigned int length) {
+  // PubSubClient.hで定義されているMQTTの最大パケットサイズ
+  char buffer[MQTT_MAX_PACKET_SIZE];
+  Serial.println("received-lastest");
 }
